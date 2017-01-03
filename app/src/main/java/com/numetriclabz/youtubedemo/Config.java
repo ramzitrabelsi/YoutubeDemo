a825b11064df97fb3d959be52c5b298cabc7a34d
ramzi <ramzi.trabelsi@it-skills.tn>
2017-01-03 22:54:38 +0100

    first commit

---
+++ app/src/main/java/com/numetriclabz/youtubedemo/Config.java
@@ -0,0 +1,12 @@
+package com.numetriclabz.youtubedemo;
+
+
+public class Config {
+
+    // Google Console APIs API key
+    // Replace this key with your key
+    public static final String API_KEY = "AIzaSyCJVFSIzx8zw8QJGh1lXnmYDL2xpMpLIMY";
+
+    // Replace this with video code you want to play
+    public static final String VIDEO_CODE = "pwkgkLNCR-s";
+}
